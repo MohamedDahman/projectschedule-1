b42a8a02ed27e6a1666d25859bb7686e7543993d
Guillermo Herrero <guillermo.herrero.i@gmail.com>
2018-01-31 10:34:16 +0100

    Add project structure

---
+++ src/main/java/at/refugeescode/projectschedule/ProjectscheduleApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.projectschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectscheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectscheduleApplication.class, args);
+	}
+}
